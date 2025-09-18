cb45c3104e1bd5a05f38b8e0b36c84a1709edc40
Anonim user <anon1mu8er@gmail.com>
2025-09-18 11:00:10 +0500

    before change database

--- src/utils/management/commands/update_district.py
+++ src/utils/management/commands/update_district.py
@@ -49,32 +49,41 @@ class Command(BaseCommand):
         #     print(neighborhood.__str__())
 
 
-        wb = load_workbook("doctors.xlsx")
-        ws = wb.active
+        # wb = load_workbook("doctors.xlsx")
+        # ws = wb.active
+        #
+        # # Merged hujayralarni tekshirish uchun
+        # merged_cells = ws.merged_cells.ranges
+        #
+        # with transaction.atomic():
+        #     for row in ws.iter_rows(min_row=1, values_only=False):
+        #         full_name = row[1].value
+        #         if full_name is None:
+        #             break
+        #         value = row[4].value
+        #         if value is None:
+        #             for merged in merged_cells:
+        #                 if row[4].coordinate in merged:
+        #                     top_left = ws[merged.coord.split(":")[0]]
+        #                     value = top_left.value
+        #                     break
+        #         Doctor.objects.create(
+        #             full_name=full_name,
+        #             birth_date=get_birth_date(row[2].value),
+        #             phone=get_phone(str(row[3].value)),
+        #             brigade_number=value,
+        #             polyclinic_name=row[5].value,
+        #             neighborhood=Neighborhood.objects.filter(district_id=13, name=row[6].value).first()
+        #         )
 
-        # Merged hujayralarni tekshirish uchun
-        merged_cells = ws.merged_cells.ranges
-
-        with transaction.atomic():
-            for row in ws.iter_rows(min_row=1, values_only=False):
-                full_name = row[1].value
-                if full_name is None:
-                    break
-                value = row[4].value
-                if value is None:
-                    for merged in merged_cells:
-                        if row[4].coordinate in merged:
-                            top_left = ws[merged.coord.split(":")[0]]
-                            value = top_left.value
-                            break
-                Doctor.objects.create(
-                    full_name=full_name,
-                    birth_date=get_birth_date(row[2].value),
-                    phone=get_phone(str(row[3].value)),
-                    brigade_number=value,
-                    polyclinic_name=row[5].value,
-                    neighborhood=Neighborhood.objects.filter(district_id=3, name=row[6].value).first()
-                )
+        neighborhoods = Neighborhood.objects.filter(doctors__isnull=True)
+        print(neighborhoods.count())
+        for neighborhood in neighborhoods:
+            doctors = neighborhood.doctors.all().count()
+            patients = neighborhood.patients.all().count()
+            print(doctors, patients)
+            if doctors == 0 and patients == 0:
+                print(neighborhood)
 
 
 
